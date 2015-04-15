2028c402027b9cb5ae22a4da4b00442a5cf934e3
Chris Lloyd <christopher.lloyd@gmail.com>
2015-04-15 16:22:15 -0700

    Added awwstn avatar.

--- app/components/avatar.js.jsx
+++ app/components/avatar.js.jsx
@@ -1,10 +1,13 @@
 import React from 'react'
 
+// In order of github-ness
 const AvatarUrls = {
   chrislloyd: 'https://avatars2.githubusercontent.com/u/718?v=3&s=460',
   whatupdave: 'https://avatars3.githubusercontent.com/u/7064?v=3&s=460',
   mdeiters:   'https://avatars3.githubusercontent.com/u/7330?v=3&s=460',
-  pletcher:   'https://avatars3.githubusercontent.com/u/2015049?v=3&s=460'
+  awwstn: 'https://avatars3.githubusercontent.com/u/1213457?v=3&s=460',
+  pletcher:   'https://avatars3.githubusercontent.com/u/2015049?v=3&s=460',
+  barisser: 'https://avatars0.githubusercontent.com/u/5825591?v=3&s=460'
 }
 
 const Avatar = React.createClass({
