a493043cda0a8e4b599c2f90ee812c4b4eb53f73
Dave Newman <dave@snappyco.de>
2015-05-04 15:52:22 -0700

    Amend previous commit with missing files

---
+++ app/lib/readraptor.js
@@ -0,0 +1,30 @@
+// This should be extracted into readraptor itself
+
+class ReadRaptor {
+  getArticle(articleId, callback) {
+    this.get(`/articles/${articleId}`, callback)
+  }
+
+  get(url, callback) {
+    var request = new XMLHttpRequest()
+    var rUrl = this.baseUrl() + url
+    request.open('GET', rUrl, true)
+
+    request.onload = () => {
+      if (request.status >= 200 && request.status < 400) {
+        var data = JSON.parse(request.responseText)
+        callback(data)
+      } else {
+      }
+    }
+    request.onerror = () => {}
+
+    request.send()
+  }
+
+  baseUrl() {
+    return 'https://readraptor.com'
+  }
+}
+
+export default new ReadRaptor()
---
+++ app/stores/story_readers_store.js
@@ -0,0 +1,39 @@
+import Dispatcher from '../lib/dispatcher'
+import readraptor from 'lib/readraptor'
+import Store from '../lib/store'
+
+class StoryReadersStore extends Store {
+  constructor() {
+    this.article = null
+
+    this.dispatchToken = Dispatcher.register(action => {
+      switch (action.type) {
+        case 'STORY_FETCHED':
+          this.article = null
+          this.emitChange()
+          readraptor.getArticle(action.story.id, a => {
+            this.article = a
+            this.emitChange()
+          })
+          break;
+      }
+    })
+  }
+
+  get totalReads() {
+    if (!this.article) {
+      return 0
+    }
+    return this.article.total_read_count
+  }
+
+  get uniqueReads() {
+    if (!this.article) {
+      return 0
+    }
+    return this.article.unique_read_count
+  }
+
+}
+
+export default new StoryReadersStore()
