5a5da8917be6176df3591db59648c8b981a85e2b
Chris Lloyd <christopher.lloyd@gmail.com>
2015-05-09 16:05:01 -0700

    Jumbotron component

---
+++ app/components/ui/jumbotron.jsx
@@ -0,0 +1,32 @@
+import React from 'react'
+import classnames from 'classnames'
+
+export default class Jumbotron extends React.Component {
+  render() {
+    const { bgColor, children, bgImageUrl } = this.props
+    const cn = classnames(
+      "bg-cover bg-no-repeat bg-center py3 sm-py4 relative",
+      `bg-${bgColor}`
+    )
+    const style = {
+      backgroundImage: `url(${bgImageUrl})`
+    }
+    return (
+      <div className={cn} style={style}>
+        <div className="absolute top-0 right-0 bottom-0 left-0 bg-darken-4" />
+        <div className="container px2 md-px0 relative">
+          {children}
+        </div>
+      </div>
+    )
+  }
+}
+
+Jumbotron.propTypes = {
+  bgColor: React.PropTypes.string.isRequired,
+  bgImageUrl: React.PropTypes.string,
+}
+
+Jumbotron.defaultProps = {
+  bgColor: 'gray'
+}
