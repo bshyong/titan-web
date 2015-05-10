0534d494905913b20560aa279a6915f0545b57e1
Chris Lloyd <christopher.lloyd@gmail.com>
2015-05-10 14:08:13 -0700

    Add username and timestamp to comments

--- app/components/comment.jsx
+++ app/components/comment.jsx
@@ -1,11 +1,12 @@
 import React from 'react'
 import Avatar from 'components/ui/avatar.jsx'
 import Markdown from 'components/ui/markdown.jsx'
+import moment from 'moment'
 
 export default class Comment extends React.Component {
   render() {
     const {
-      comment: {user, body}
+      comment: {user, body, created_at}
     } = this.props
 
     return (
@@ -14,7 +15,11 @@ export default class Comment extends React.Component {
           <Avatar user={user} size={24} />
         </div>
         <div className="flex-auto">
-          <h4 className="mt0 mb1">{user.username}</h4>
+          <h4 className="mt0 mb1">
+            {user.username}
+            {' '}
+            <span className="gray regular">{moment(created_at).fromNow(true)}</span>
+          </h4>
           <Markdown markdown={body} />
         </div>
       </div>
--- app/config/moment.js
+++ app/config/moment.js
@@ -1,12 +1,27 @@
 import moment from 'moment'
 
 moment.locale('en', {
-    calendar : {
-        lastDay : '[Yesterday]',
-        sameDay : '[Today]',
-        nextDay : '[Tomorrow]',
-        lastWeek : 'dddd',
-        nextWeek : 'dddd [at]',
-        sameElse : 'LL'
-    }
+  calendar : {
+    lastDay : '[Yesterday]',
+    sameDay : '[Today]',
+    nextDay : '[Tomorrow]',
+    lastWeek : 'dddd',
+    nextWeek : 'dddd [at]',
+    sameElse : 'LL'
+  },
+  relativeTime : {
+      future: "in %s",
+      past:   "%s ago",
+      s:  "seconds",
+      m:  "a minute",
+      mm: "%dm",
+      h:  "an hour",
+      hh: "%dh",
+      d:  "a day",
+      dd: "%dd",
+      M:  "a month",
+      MM: "%dm",
+      y:  "a year",
+      yy: "%dy"
+  }
 })
