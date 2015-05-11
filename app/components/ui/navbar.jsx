f2afc0356bc86b1c8430a3b455e54ec02e722d12
Charles Pletcher <cpletcher2@gmail.com>
2015-05-11 13:06:02 -0700

    Use Navbar's title prop to set document.title

--- app/components/ui/navbar.jsx
+++ app/components/ui/navbar.jsx
@@ -1,6 +1,14 @@
 import React from 'react'
 
 export default class Navbar extends React.Component {
+  componentDidMount() {
+    document.title = this.props.title || document.title
+  }
+
+  componentDidUpdate() {
+    document.title = this.props.title || document.title
+  }
+
   render() {
     const {title, left, right} = this.props
     return <div className="navbar flex relative z1">
