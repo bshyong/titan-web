c3b86226a16ce3c0f141b9993905e8796cc984d8
barisser <barisser@gmail.com>
2015-05-11 16:16:03 -0700

    kill console logs

--- app/components/changelog.js.jsx
+++ app/components/changelog.js.jsx
@@ -23,7 +23,6 @@ export default class Changelog extends React.Component {
     StoryActions.fetchAll(params.changelogId)
   }
   static get defaultProps() {
-    console.log(RouterContainer.get().getCurrentParams())
     return {
       changelogId: RouterContainer.get().getCurrentParams().changelogId
     }
