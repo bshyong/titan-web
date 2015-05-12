45c1dd12004ac810d20cb61b963881c8b920d48f
Dave Newman <dave@snappyco.de>
2015-05-12 13:41:53 -0700

    purge all pages post deploy

--- post-deploy.js
+++ post-deploy.js
@@ -1,6 +1,6 @@
 var fastly = require('fastly')(process.env.FASTLY_API_KEY);
 
-fastly.purge('titan-web.herokuapp.com', '/', function(err, obj) {
+fastly.purgeAll(process.env.FASTLY_SERVICE_ID, function(err, obj) {
   if (err) return console.dir(err);   // Oh no!
   console.dir(obj);                   // Response body from the fastly API
 });
