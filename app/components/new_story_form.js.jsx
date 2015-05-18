ce297266cd79ffb4ece731fcf808567a626fbd1b
barisser <barisser@gmail.com>
2015-05-18 14:05:56 -0700

    made new stories public by default

--- app/components/new_story_form.js.jsx
+++ app/components/new_story_form.js.jsx
@@ -42,7 +42,8 @@ export default AuthenticatedMixin(class NewStoryForm extends React.Component {
 
   static get defaultProps() {
     return {
-      changelogId: RouterContainer.get().getCurrentParams().changelogId
+      changelogId: RouterContainer.get().getCurrentParams().changelogId,
+      isPublic: true
     }
   }
 
@@ -152,6 +153,8 @@ export default AuthenticatedMixin(class NewStoryForm extends React.Component {
   }
 
   _handleTogglePrivacy() {
+    console.log(this.state.isPublic)
+    console.log(StoryFormStore.isPublic)
     this.updateForm('isPublic', !this.state.isPublic)
   }
 
