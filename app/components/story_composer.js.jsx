de26601a7523261f833919088512fea8d075d0a6
Dave Newman <dave@snappyco.de>
2015-05-22 12:07:42 -0700

    Only allow core team access to story composer

--- app/components/story_composer.js.jsx
+++ app/components/story_composer.js.jsx
@@ -1,12 +1,33 @@
 import { RouteHandler, Link } from 'react-router'
 import ChangelogStore from '../stores/changelog_store'
+import connectToStores from '../lib/connectToStores.jsx'
 import HighlightsActionCreator from '../actions/highlight_actions'
 import React from 'react'
 import RouterContainer from '../lib/router_container'
 
+@connectToStores(ChangelogStore)
 export default class StoryComposer extends React.Component {
   static willTransitionTo(transition, params, query) {
     HighlightsActionCreator.fetchAll(params.changelogId)
+  }
+
+  static getPropsFromStores(props) {
+    return { changelog: ChangelogStore.changelog }
+  }
+
+  componentWillMount() {
+    this.coreTeamOnly(this.props)
+  }
+
+  componentWillReceiveProps(next) {
+    this.coreTeamOnly(next)
+  }
+
+  coreTeamOnly(props) {
+    if (props.changelog && !props.changelog.user_is_team_member) {
+      setTimeout(() => RouterContainer.get().transitionTo('/'), 1)
+    }
+
   }
 
   render() {
