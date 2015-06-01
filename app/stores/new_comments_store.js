ed58a1a4500f1dbd1c3148fc37287b78b4886086
Ben <hello@benshyong.com>
2015-06-01 12:09:38 -0700

    add optimistic comments

--- app/stores/new_comments_store.js
+++ app/stores/new_comments_store.js
@@ -14,27 +14,42 @@ class NewCommentsStore extends Store {
     this.dispatchToken = Dispatcher.register((action) => {
       switch (action.type) {
         case COMMENT_CREATING:
-          this.comments = this.comments.delete(action.storyId)
+          this.comments.get(action.storyId).isSaving = true
           this.emitChange()
           break;
         case COMMENT_FORM_CHANGE:
-          this.comments = this.comments.set(action.storyId, action.comment)
+          this.comments = this.comments.set(action.storyId, {body: action.comment})
           this.emitChange()
           break;
         case COMMENT_PUBLISHED:
+          this.comments = this.comments.delete(action.storyId)
+          this.emitChange()
+          break
         default:
           break;
       }
     })
   }
 
+  isSaving(storyId) {
+    const comment = this.comments.get(storyId)
+    if (comment) {
+      return this.comments.get(storyId).isSaving
+    }
+  }
+
   isValid(storyId) {
     const comment = this.comments.get(storyId)
-    return comment && comment.length > 0
+    if (comment) {
+      return comment.body && comment.body.length > 0
+    }
   }
 
   get(storyId) {
-    return this.comments.get(storyId)
+    const comment = this.comments.get(storyId)
+    if (comment) {
+      return comment.body
+    }
   }
 
   all() {
