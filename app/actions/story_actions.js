6590b7f15b11626255c20a335e4b77eab77f6a50
Ben <hello@benshyong.com>
2015-06-07 00:37:15 -0700

    clickable pagination working except for one buggy case: clicking into a full month/week page and then going back to the home page causes the 'show more' bar to be in the wrong state

--- app/actions/story_actions.js
+++ app/actions/story_actions.js
@@ -43,11 +43,11 @@ export default {
       })
   },
 
-  fetchSpecificDate(changelogId, dateString, timeInterval) {
+  fetchSpecificDate(changelogId, dateString, timeInterval, page, per) {
     Dispatcher.dispatch({
       type: STORIES_FETCHING
     })
-    api.get(`changelogs/${changelogId}/stories?date=${dateString}&time_length=${timeInterval}`).
+    api.get(`changelogs/${changelogId}/stories?date=${dateString}&time_length=${timeInterval}&page=${page}&per=${per}`).
       then(resp => {
         let stories = List(resp)
         Dispatcher.dispatch({
