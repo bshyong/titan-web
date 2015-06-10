17464dd4e8b3fe1ec95763056dd7786a5e29271b
Ben <hello@benshyong.com>
2015-06-09 17:54:15 -0700

    show first letter of changelog if there is no logourl

--- app/components/logo.jsx
+++ app/components/logo.jsx
@@ -9,9 +9,18 @@ export default class Logo extends React.Component {
       height: size,
       outline: 'none'
     }
+    if (src) {
+      return (
+        <div className="bg-silver rounded" style={style}>
+          <img className="block rounded" src={src} style={style} alt={name} />
+        </div>
+      )
+    }
     return (
-      <div className="bg-silver rounded" style={style}>
-        <img className="block rounded" src={src} style={style} alt={name} />
+      <div className="bg-smoke rounded flex flex-center" style={style}>
+        <div className="black h1 mx-auto bold">
+          {name.charAt(0).toUpperCase()}
+        </div>
       </div>
     )
   }
