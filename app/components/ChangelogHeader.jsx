ddb0b1d15738331aaa3e489904c63ca34cdb481f
Chris Lloyd <christopher.lloyd@gmail.com>
2015-06-17 14:38:08 -0700

    Add homepage URL to the changeling header.

--- app/components/ChangelogHeader.jsx
+++ app/components/ChangelogHeader.jsx
@@ -6,6 +6,7 @@ import Logo from './logo.jsx'
 import React from 'react'
 import connectToStores from '../lib/connectToStores.jsx'
 import { Link } from 'react-router'
+import URL from 'url'
 
 @connectToStores(ChangelogStore)
 export default class ChangelogHeader extends React.Component {
@@ -22,12 +23,15 @@ export default class ChangelogHeader extends React.Component {
       <Jumbotron bgColor="charcoal" bgImageUrl={changelog.banner_url}>
         <div className="sm-flex flex-center">
           <div className="flex-none mb2 sm-mb0">
-            <div className="mx-auto" style={{width: '4rem'}}><Logo changelog={changelog} size="4rem"/></div>
+            <Link className="block mx-auto" style={{width: '4rem'}} to="changelog" params={{changelogId}}>
+              <Logo changelog={changelog} size="4rem"/>
+            </Link>
           </div>
-          <Link className="block flex-auto mb2 md-mb0 sm-px3 center sm-left-align white" to="changelog" params={{changelogId}}>
-            <h2 className="mt0 mb0">{changelog.name}</h2>
+          <div className="block flex-auto mb2 md-mb0 sm-px3 center sm-left-align white">
+            <h2 className="mt0 mb0 bold">{changelog.name}</h2>
             <div>{changelog.tagline}</div>
-          </Link>
+            {this.renderHomepageUrl()}
+          </div>
           <div className="flex-none sm-ml2">
             <div className="flex mxn1">
               {this.renderNewStoryButton()}
@@ -39,6 +43,19 @@ export default class ChangelogHeader extends React.Component {
           </div>
         </div>
       </Jumbotron>
+    )
+  }
+
+  renderHomepageUrl() {
+    const { changelog } = this.props
+    if (!changelog.homepage_url) {
+      return
+    }
+    const host = URL.parse(changelog.homepage_url).host
+    return (
+      <div>
+        <a className="white" href={changelog.homepage_url} style={{textDecoration: "underline"}}>{host}</a>
+      </div>
     )
   }
 
