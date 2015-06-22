17d79fa6061e9dafed56e9e4270c125a82a4d64d
Ben <hello@benshyong.com>
2015-06-22 14:05:56 -0700

    add copy on privacy settings

--- app/components/ChangelogCreation.jsx
+++ app/components/ChangelogCreation.jsx
@@ -63,21 +63,28 @@ export default class ChangelogCreation extends React.Component {
       <div className="flex flex-center py2">
         <div className="flex-auto">
           <h4 className="mt0 mb0">Choose who can see your Changelog</h4>
+          <h4 className="mt0 mb1 gray">You can change this setting later.</h4>
           <RadioGroup name="privacy"
             selectedValue={is_members_only ? 'private' : 'public'}
             onChange={this.toggleVisibility.bind(this)}>
             {Radio => (
               <div>
-                <div>
-                  <label>
-                    <Radio value="public" ref="public" />
-                    Public <span className="gray">(Anyone with link)</span>
+                <div className="mb1">
+                  <label className="flex">
+                    <div className="flex-none mr1"><Radio value="public" ref="public" /></div>
+                    <div>
+                      <h4 className="m0">Public</h4>
+                      <p className="mb0 gray">Anyone wil be able to see it, follow it, and comment on it.</p>
+                    </div>
                   </label>
                 </div>
                 <div>
-                  <label>
-                    <Radio value="private" ref="private" />
-                    Private <span className="gray">(Only invited members)</span>
+                  <label className="flex">
+                    <div className="flex-none mr1"><Radio value="private" ref="private" /></div>
+                    <div>
+                      <h4 className="m0">Private</h4>
+                      <p className="mb0 gray">Only those you invite will be able to see and comment on it.</p>
+                    </div>
                   </label>
                 </div>
               </div>
