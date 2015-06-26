29cd622e249d2d3cb0f7900fd618b52dec2f0867
Ben <hello@benshyong.com>
2015-06-26 16:30:00 -0700

    github repo actions

--- app/pages/GithubRepoSelectionPage.jsx
+++ app/pages/GithubRepoSelectionPage.jsx
@@ -9,19 +9,27 @@ import SessionStore from '../stores/session_store'
 export default class GithubRepoSelectionPage extends React.Component {
   static willTransitionTo(transition, params, query) {
     const user = SessionStore.user
-    if (!user) { SessionActions.signin() }
+    if (user){ GithubRepoActions.fetchAll() }
+    else { SessionActions.signin() }
   }
 
   render() {
     const user = SessionStore.user
     return <div>
       <ApplicationNavbar />
-      { true ? this.renderAuthedState() : this.renderUnauthedState() }
+      <div className="container">
+        { true ? this.renderAuthedState() : this.renderUnauthedState() }
+      </div>
     </div>
   }
 
   renderAuthedState() {
-
+    return (
+      <div>
+        <h3>Choose a repo</h3>
+        {GithubReposStore.repos.map(r => {return r})}
+      </div>
+    )
   }
 
   renderUnauthedState() {
