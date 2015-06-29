a3692d9a9914e3a0cee288017588843a07ea9d4a
Charles Pletcher <cpletcher2@gmail.com>
2015-06-29 13:45:35 -0400

    Remove banners about v1 logins; debounce username/email availability check

--- app/actions/AvailableUsernameInputActions.js
+++ app/actions/AvailableUsernameInputActions.js
@@ -2,25 +2,28 @@ import {
   AVAILABLE_USERNAME_INPUT_VALIDATED
 } from 'constants'
 import api from 'lib/api'
+import debounce from 'lib/debounce'
 import Dispatcher from 'lib/dispatcher'
 
-export default {
-  validate(id, username) {
-    if (!username) {
-      return setTimeout(() => {
-          Dispatcher.dispatch({
-          type: AVAILABLE_USERNAME_INPUT_VALIDATED,
-          id: id,
-          valid: null
-        })
-      }, 10)
-    }
-    api.get(`users/validate_username?s=${username}`).then(resp => {
-      Dispatcher.dispatch({
-        type: AVAILABLE_USERNAME_INPUT_VALIDATED,
-        id: id,
-        valid: resp.valid
-      })
+const validate = (id, username) => {
+  if (!username) {
+    return Dispatcher.dispatch({
+      type: AVAILABLE_USERNAME_INPUT_VALIDATED,
+      id: id,
+      valid: null
     })
+  }
+  api.get(`users/validate_username?s=${username}`).then(resp => {
+    Dispatcher.dispatch({
+      type: AVAILABLE_USERNAME_INPUT_VALIDATED,
+      id: id,
+      valid: resp.valid
+    })
+  })
+}
+
+export default {
+  validate: (id, username) => {
+    debounce(validate, this, [id, username])()
   }
 }
--- app/components/Authentication/LoginForm.jsx
+++ app/components/Authentication/LoginForm.jsx
@@ -32,68 +32,53 @@ export default class LoginForm extends React.Component {
     } = this.props
 
     return (
-      <div>
-        <div className="flex flex-center px4">
-          <div className="bg-orange white p2 rounded">
-            Have an account on Assembly v1? We've transferred it over,
-            but you'll need to create a new password. Click
-            {' '}
-            <a href="javascript:void(0)"
-              onClick={this.handleForgotPassword}
-              className="white underline">
-              here
-            </a>
-            {' '}to do that.
-          </div>
-        </div>
-        <div className="flex flex-center">
-          <div className="flex-none col-4 mx-auto py4">
-            <img className="flex-none mr2" src={LogoSrc} style={{height: '1.5rem'}} />
-            <h1 className="mt0">Log in</h1>
-            <Button size="big" color="twitter-blue" block
-              action={SessionActions.initializeTwitterSignIn}>
-              <Icon icon="twitter" />
-              <span className="ml2">Use Twitter</span>
-            </Button>
+      <div className="flex flex-center">
+        <div className="flex-none col-4 mx-auto py4">
+          <img className="flex-none mr2" src={LogoSrc} style={{height: '1.5rem'}} />
+          <h1 className="mt0">Log in</h1>
+          <Button size="big" color="twitter-blue" block
+            action={SessionActions.initializeTwitterSignIn}>
+            <Icon icon="twitter" />
+            <span className="ml2">Use Twitter</span>
+          </Button>
 
-            <div className="mt2">
-              <div className="border border-silver rounded p2">
-                <AuthenticationFormError />
-                <form className="clearfix">
-                  <div className="py1">
-                    <label className="left bold">Username</label>
-                    <input type="text"
-                      className="block full-width field-light"
-                      placeholder="jane"
-                      value={username}
-                      onChange={this.handleChange('username')}  />
-                  </div>
+          <div className="mt2">
+            <div className="border border-silver rounded p2">
+              <AuthenticationFormError />
+              <form className="clearfix">
+                <div className="py1">
+                  <label className="left bold">Username</label>
+                  <input type="text"
+                    className="block full-width field-light"
+                    placeholder="jane"
+                    value={username}
+                    onChange={this.handleChange('username')}  />
+                </div>
 
-                  <div className="py1">
-                    <label className="left bold block">Password</label>
-                    <input type="password"
-                      className="block full-width field-light"
-                      value={password}
-                      onChange={this.handleChange('password')} />
-                    <small className="left">
-                      <a href="javascript:void(0)"
-                        className="gray underline"
-                        onClick={this.handleForgotPassword}>
-                        Forgot password?
-                      </a>
-                    </small>
-                  </div>
+                <div className="py1">
+                  <label className="left bold block">Password</label>
+                  <input type="password"
+                    className="block full-width field-light"
+                    value={password}
+                    onChange={this.handleChange('password')} />
+                  <small className="left">
+                    <a href="javascript:void(0)"
+                      className="gray underline"
+                      onClick={this.handleForgotPassword}>
+                      Forgot password?
+                    </a>
+                  </small>
+                </div>
 
-                  <div className="py2 mt2">
-                    <Button size="big"
-                      color="black bg-darken-2"
-                      block
-                      action={this.handleSubmit}>
-                      Log in
-                    </Button>
-                  </div>
-                </form>
-              </div>
+                <div className="py2 mt2">
+                  <Button size="big"
+                    color="black bg-darken-2"
+                    block
+                    action={this.handleSubmit}>
+                    Log in
+                  </Button>
+                </div>
+              </form>
             </div>
           </div>
         </div>
