ccf4c92c0032f6b2aecf99c44b4d74af721b24e3
Chris Lloyd <christopher.lloyd@gmail.com>
2015-07-06 14:33:41 -0700

    Tweaks to the dashboard page
    * Works with new API methods
    * Linted a few things…

--- app/pages/FeedPage.jsx
+++ app/pages/FeedPage.jsx
@@ -1,33 +1,20 @@
 import React from 'react'
-import RouterContainer from '../lib/router_container'
 import ApplicationNavbar from '../components/application_navbar.jsx'
-import SessionActions from '../actions/SessionActions'
-import SessionStore from '../stores/session_store'
 import StoryActions from '../actions/story_actions'
 import StoryFeed from '../components/StoryFeed.jsx'
 
 export default class FeedPage extends React.Component {
-
   static willTransitionTo(transition, params, query) {
-    let user = SessionStore.user
-    if (user !== null) {
-      let username = user.username
-      StoryActions.fetchUserFirehoseFeed(username, 1, 25)
-    }
+    StoryActions.fetchFeed(1, 25)
   }
 
   render() {
-    if (!SessionStore.isSignedIn()) {
-      SessionActions.signin()
-      return (<div/>)
-    } else {
-      return <div>
-        <ApplicationNavbar />
-        <div className="container">
-          <h1 className="caps border-bottom center py2 mt4 mb4">The <span className="blue">Long</span> Log</h1>
-          <StoryFeed />
-        </div>
+    return <div>
+      <ApplicationNavbar />
+      <div className="container">
+        <h1 className="caps border-bottom center py2 mt4 mb4">The <span className="blue">Long</span> Log</h1>
+        <StoryFeed />
       </div>
-    }
+    </div>
   }
 }
