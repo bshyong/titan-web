fe5840d76939df2a462d7370ba1578802fb379a6
Chris Lloyd <christopher.lloyd@gmail.com>
2015-07-07 16:25:17 -0700

    Fix homepage sticky-ness in Safari.

--- app/ui/Sticky.jsx
+++ app/ui/Sticky.jsx
@@ -4,7 +4,7 @@ import ReactStickyPosition from 'react-sticky-position'
 export default class Stick extends React.Component {
   render() {
     return (
-      <ReactStickyPosition classname="sticky top-0">
+      <ReactStickyPosition className="sticky top-0">
         {this.props.children}
       </ReactStickyPosition>
     )
