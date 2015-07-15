2549c763dec69cc2c3c69c81e8cfda322a95be07
Ben <hello@benshyong.com>
2015-07-15 11:02:12 -0700

    typo fix

--- app/components/changelog.js.jsx
+++ app/components/changelog.js.jsx
@@ -135,7 +135,7 @@ export default class Changelog extends React.Component {
     if (totalStoriesCount > 0 || !changelog.user_is_team_member || loading) { return }
 
     return <div className="mt3 mb3 p2 bg-smoke h4 sm-flex flex-center flex-wrap">
-      <div className="flex-auto">You can pull in draft posts from your GitHub repos.<br /><span className="h5 gray">We dont't save your data and promise not to peek at your code.</span></div>
+      <div className="flex-auto">You can pull in draft posts from your GitHub repos.<br /><span className="h5 gray">We don't save your data and promise not to peek at your code.</span></div>
       <div className="flex-none py2">
         <a href={`${API_URL}/auth/github?origin=${window.location.origin}${Router.get().makeHref('githubRepos', {changelogId})}`}><Button block={true}>Connect GitHub</Button></a>
       </div>
