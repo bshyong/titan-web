607b73c37f5871ee62293c9d0cfa370daee66924
Charles Pletcher <cpletcher2@gmail.com>
2015-07-23 17:31:08 -0700

    Redux admin store

--- app/pages/AdminPage.jsx
+++ app/pages/AdminPage.jsx
@@ -1,38 +1,29 @@
 import React from 'react'
-import Admin from '../components/Admin.jsx'
-import AdminActions from '../actions/admin_actions'
-import AuthenticatedComponent from '../components/mixins/authenticated_mixin.jsx'
-import RouterContainer from '../lib/router_container'
+import Admin from 'components/Admin.jsx'
+import AdminActions from 'actions/admin_actions'
+import authenticated from 'components/mixins/authenticated_mixin.jsx'
 import AppNavbar from 'components/App/AppNavbar.jsx'
-import SessionActions from '../actions/SessionActions'
-import SessionStore from '../stores/session_store'
+import SessionStore from 'stores/session_store'
 
-@AuthenticatedComponent()
+@authenticated()
 export default class AdminPage extends React.Component {
-
-  static willTransitionTo() {
-    AdminActions.adminDataFetched()
-    AdminActions.adminUserDataFetched()
-    AdminActions.adminStoriesFetched()
-    AdminActions.adminStatsFetched()
-  }
-
-  static get defaultProps() {
-    return {
-      user: SessionStore.user
+  static willTransitionTo(transition) {
+    if (SessionStore.user && SessionStore.user.staff_at) {
+      AdminActions.adminDataFetched()
+      AdminActions.adminUserDataFetched()
+      AdminActions.adminStoriesFetched()
+      AdminActions.adminStatsFetched()
+    } else {
+      transition.redirect('home')
     }
   }
 
   render() {
-    if (this.props.user.staff_at!==null) {
-      return (
-        <div>
-          <AppNavbar />
-          <Admin/>
-        </div>
-      )
-    } else {
-      RouterContainer.get().transitionTo('/home')
-    }
+    return (
+      <div>
+        <AppNavbar />
+        <Admin/>
+      </div>
+    )
   }
 }
