3dc4692b1ecdf118524a472b5a6ec2355601d219
barisser <barisser@gmail.com>
2015-07-30 16:07:20 -0700

    only show followers on graph, change last activity to last viewed at

--- app/pages/GroupAdminPage.jsx
+++ app/pages/GroupAdminPage.jsx
@@ -77,33 +77,19 @@ export class GroupAdminPage extends React.Component {
         data: {
           xs: {
               'Followers': 'x',
-              'Hearts': 'x2',
-              'Views': 'x3'
           },
           columns: [
             d,
-            d2,
-            d3,
             n,
-            h,
-            v
             ],
           type: 'area-spline',
           axes: {
-            'Hearts': 'y2',
-            'Followers': 'y',
-            'Views': 'y3'
+            'Followers': 'y'
           }
         },
         axis: {
           x: {
            type: 'timeseries'
-         },
-         y2: {
-           show: true
-         },
-         y3: {
-           show: true
          }
           }
       });
@@ -180,7 +166,7 @@ export class GroupMembers extends React.Component {
                   onClick={sort => fetchMembers(changelogId, 1, per, sort, filter)}
                   activeCategory={sortCategory}
                   direction={sortOrder || 'desc'} />
-                 &nbsp;Last Activity
+                &nbsp;Last Viewed At
               </th>
               <th className="">
                 <SortArrow
